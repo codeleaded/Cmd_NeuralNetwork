f779dc69effa5f77129df2be2b75261823b54a2f
codeleaded <codeleaded@gmail.com>
2025-11-06 18:18:56 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -1,11 +1,20 @@
 #include "/home/codeleaded/System/Static/Library/NeuralNetwork.h"
 
+#define NN_PATH             "./data/Output.nnalx"
+#define NN_LEARNRATE        0.1f
+
 int main(int argc,char** argv){
     
-    NeuralNetwork nn = NeuralNetwork_Make((NeuralLayerCount[]){ 3,2,3,NeuralLayerCount_End });
-    NeuralNetwork_SetInput(&nn,(NeuralType[]){ 1.0f,1.0f,1.0f });
-    NeuralNetwork_Calculate(&nn);
-    NeuralNetwork_Print_G(&nn);
+    NeuralNetwork nn = NeuralNetwork_Make(
+        (NeuralLayerBuilder[]){ 
+            NeuralLayerBuilder_Make(2,"relu"),
+            NeuralLayerBuilder_Make(3,"relu"),
+            NeuralLayerBuilder_Make(2,"softmax"),
+            NeuralLayerBuilder_End()
+        }
+    );
+    NeuralNetwork_Calc(&nn,(NeuralType[]){ 1.0f,1.0f,1.0f });
+    NeuralNetwork_Save(&nn,NN_PATH);
     NeuralNetwork_Free(&nn);
 
     return 0;
